90cf80cc751db242806e7ecf7e43cfca989548cf
learnsrt-collab <learnsrt@gmail.com>
2026-04-23 21:50:01 +0530

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,10 @@
+quiz_data = {
+    "question": "CPU stands for?",
+    "options": [
+        "Central Process Unit",
+        "Central Processing Unit",
+        "Computer Power Unit",
+        "Control Power Unit"
+    ],
+    "answer": "Central Processing Unit"
+}
